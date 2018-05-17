db5111f6948ddb1049621c2e5511adafc3d5e248
RondonLeonR <RondonLeonR@github.com>
2018-05-17 11:11:47 -0300

    Funciones con Punteros 2

--- FuncionesPunteros/main.c
+++ FuncionesPunteros/main.c
@@ -2,17 +2,24 @@
 #include <stdlib.h>
 void Intercambiar(int num1, int num2);
 void IntercambiarConPunteros(int *num1, int *num2);
+void PonerEnCero(int *num1);
+
 int main()
 {
     int edadUno;
     int edadDos;
     edadUno=22;
     edadDos=99;
+    PonerEnCero(&edadDos);
     //Intercambiar(edadUno,edadDos);
     IntercambiarConPunteros(&edadUno,&edadDos);
     printf("\n\t Edad 1:%d\n",edadUno);
     printf("\n\t Edad 2:%d\n",edadDos);
     return 0;
+}
+void PonerEnCero(int *num1)
+{
+    *num1=0;
 }
 
 void Intercambiar(int num1, int num2)
